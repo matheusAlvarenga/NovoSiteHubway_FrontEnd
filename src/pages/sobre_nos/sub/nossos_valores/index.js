e06eb6b02af05a24b74fc40e608824bb307f2ad8
matheusAlvarenga <gop.canal@gmail.com>
2020-10-06 13:01:50 -0300

    Finished the dynamic Why Choose Us in About Us

--- src/pages/sobre_nos/sub/nossos_valores/index.js
+++ src/pages/sobre_nos/sub/nossos_valores/index.js
@@ -1,8 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import './style.css';
 
 export default function NossosValores({ dados, textos }) {
+    const [escolhido, setEscolhido] = useState({
+        id: 0,
+        paragrafo: dados[0].texto,
+        imagem: dados[0].imagem,
+    });
+
+    function troca(key, texto, img) {
+        setEscolhido({
+            id: key,
+            paragrafo: texto,
+            imagem: img,
+        });
+    }
+
     return (
         <div className="nossos-valores-container">
             <div className="header">
@@ -11,39 +25,30 @@ export default function NossosValores({ dados, textos }) {
                 <p className="paragrafo">{textos.paragrafo}</p>
             </div>
             <div className="lista-titulo">
-                <div className="titulos">
-                    <p>Lorem Ipsum</p>
-                </div>
-                <div className="titulos active">
-                    <p>Lorem Ipsum</p>
-                </div>
-                <div className="titulos">
-                    <p>Lorem Ipsum</p>
-                </div>
+                {dados.map((tit, key) => {
+                    return (
+                        <div
+                            onClick={(titu) =>
+                                troca(key, dados[key].texto, dados[key].imagem)
+                            }
+                            className={
+                                escolhido.id === key
+                                    ? 'titulos active'
+                                    : 'titulos'
+                            }
+                            key={key}
+                        >
+                            <p>{tit.titulo}</p>
+                        </div>
+                    );
+                })}
             </div>
             <div className="corpo">
                 <div className="imagem">
-                    <img src="https://images.unsplash.com/photo-1601919263076-4a6a8514c461" />
+                    <img src={escolhido.imagem} />
                 </div>
                 <div className="paragrafo">
-                    <p>
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                        Suspendisse interdum neque eu est ultricies, sit amet
-                        convallis odio bibendum. In consectetur porttitor
-                        semper. Phasellus vestibulum quis urna vel fermentum.
-                        Morbi placerat condimentum urna id convallis. Duis in
-                        arcu velit. Pellentesque dictum nisl enim, at
-                        consectetur lacus lacinia vel. Cras ipsum eros, aliquet
-                        et diam quis, fringilla molestie est. Nulla placerat
-                        nisi eget lorem auctor consequat. In hac habitasse
-                        platea dictumst. Nullam iaculis augue at metus feugiat
-                        fringilla. Pellentesque habitant morbi tristique
-                        senectus et netus et malesuada fames ac turpis egestas.
-                        Cras suscipit non urna sit amet sagittis.<br></br> Ut
-                        dignissim sapien sapien, elementum tincidunt odio
-                        rhoncus vitae. Praesent eu augue sodales, aliquet arcu
-                        non, hendrerit justo.
-                    </p>
+                    <p>{escolhido.paragrafo}</p>
                 </div>
             </div>
         </div>
