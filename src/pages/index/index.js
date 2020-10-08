5d7a8ac38ba6fd1b9cd1f7284344e7de6d28cbf2
matheusAlvarenga <gop.canal@gmail.com>
2020-10-08 10:25:18 -0300

    Finisged the Services And Start Banner to Be Responsive

--- src/pages/index/index.js
+++ src/pages/index/index.js
@@ -1,16 +1,30 @@
 import React, { Component } from 'react';
 import ScrollLock from 'react-scrolllock';
+import { useMediaQuery } from 'react-responsive';
 
 import Header from '../header';
 import HeaderMobile from '../header_mobile';
+
 import VideoPrincipal from './sub/video_principal';
+import VideoPrincipalMobile from './sub/video_principal_mobile';
+
 import Servicos from './sub/servicos';
+import ServicosMobile from './sub/servicos_mobile';
+import ServicosTablet from './sub/servicos_tablet';
+import ServicosDesktopSmall from './sub/servicos_desktop_small';
+
 import SobreNos from './sub/sobre_nos';
+
 import Contadores from './sub/contadores';
+
 import BannerFaleConosco from '../banner_fale_conosco';
+
 import Tecnologias from './sub/tecnologias';
+
 import Feedback from '../feedback';
+
 import FormFaleConosco from '../form_fale_conosco';
+
 import Footer from '../footer';
 import FooterMobile from '../footer_mobile';
 
@@ -21,6 +35,42 @@ import contadores_obj from '../../shared/contadores_model.json';
 import tecnologias_obj from '../../shared/tecnologias_model.json';
 import feedbacks_obj from '../../shared/feedbacks_model.json';
 import index_obj from '../../shared/index.json';
+
+const Mobile = ({ children }) => {
+    const isMobile = useMediaQuery({ maxWidth: 700 });
+    return isMobile ? children : null;
+};
+
+const MobileBig = ({ children }) => {
+    const isMobile = useMediaQuery({ minWidth: 701, maxWidth: 799 });
+    return isMobile ? children : null;
+};
+
+const NoMobile = ({ children }) => {
+    const isNoMobile = useMediaQuery({ minWidth: 800 });
+    return isNoMobile ? children : null;
+};
+
+const Tablet = ({ children }) => {
+    const isTablet = useMediaQuery({ minWidth: 800, maxWidth: 950 });
+    return isTablet ? children : null;
+};
+
+const DesktopSmall = ({ children }) => {
+    const isDesktopSmall = useMediaQuery({ minWidth: 951, maxWidth: 1220 });
+    return isDesktopSmall ? children : null;
+};
+
+const DesktopNormal = ({ children }) => {
+    const isDesktopNormal = useMediaQuery({ minWidth: 1221, maxWidth: 1500 });
+    return isDesktopNormal ? children : null;
+};
+
+const DesktopBig = ({ children }) => {
+    const isDesktopBig = useMediaQuery({ minWidth: 1501 });
+    return isDesktopBig ? children : null;
+};
+
 export default class Index extends Component {
     constructor(props) {
         super(props);
@@ -40,14 +90,85 @@ export default class Index extends Component {
     render() {
         return (
             <div id="outer">
-                {/* <Header></Header> */}
-                <HeaderMobile></HeaderMobile>
+                {/* HEADER */}
+
+                <Mobile>
+                    <HeaderMobile></HeaderMobile>
+                </Mobile>
+
+                <MobileBig>
+                    <HeaderMobile></HeaderMobile>
+                </MobileBig>
+
+                <NoMobile>
+                    <Header></Header>
+                </NoMobile>
+
+                {/* FIM HEADER */}
+
                 <div id="wrap" className="index-container">
-                    <VideoPrincipal></VideoPrincipal>
-                    <Servicos
-                        dados={this.finalServicos}
-                        textos={this.textoServicos}
-                    ></Servicos>
+                    {/* VIDEO PRINCIPAL */}
+
+                    <Mobile>
+                        <VideoPrincipalMobile></VideoPrincipalMobile>
+                    </Mobile>
+
+                    <MobileBig>
+                        <VideoPrincipalMobile></VideoPrincipalMobile>
+                    </MobileBig>
+
+                    <NoMobile>
+                        <VideoPrincipal></VideoPrincipal>
+                    </NoMobile>
+
+                    {/* FIM VIDEO PRINCIPAL */}
+
+                    {/* SERVICOS */}
+
+                    <Mobile>
+                        <ServicosMobile
+                            dados={this.finalServicos}
+                            textos={this.textoServicos}
+                        ></ServicosMobile>
+                    </Mobile>
+
+                    <MobileBig>
+                        <ServicosTablet
+                            dados={this.finalServicos}
+                            textos={this.textoServicos}
+                        ></ServicosTablet>
+                    </MobileBig>
+
+                    <Tablet>
+                        <ServicosTablet
+                            dados={this.finalServicos}
+                            textos={this.textoServicos}
+                        ></ServicosTablet>
+                    </Tablet>
+
+                    <DesktopSmall>
+                        <ServicosDesktopSmall
+                            dados={this.finalServicos}
+                            textos={this.textoServicos}
+                        ></ServicosDesktopSmall>
+                    </DesktopSmall>
+
+                    <DesktopNormal>
+                        <Servicos
+                            dados={this.finalServicos}
+                            textos={this.textoServicos}
+                        ></Servicos>
+                    </DesktopNormal>
+
+                    <DesktopBig>
+                        <Servicos
+                            dados={this.finalServicos}
+                            textos={this.textoServicos}
+                        ></Servicos>
+                    </DesktopBig>
+
+                    {/* FIM SERVICOS */}
+
                     <SobreNos textos={this.textoSobreNos}></SobreNos>
                     <Contadores
                         dados={this.contadores}
@@ -68,8 +189,22 @@ export default class Index extends Component {
                         textos={this.textoFaleConosoc}
                     ></FormFaleConosco>
                 </div>
-                {/* <Footer></Footer> */}
-                <FooterMobile></FooterMobile>
+
+                {/* FOOTER */}
+
+                <Mobile>
+                    <FooterMobile></FooterMobile>
+                </Mobile>
+
+                <MobileBig>
+                    <FooterMobile></FooterMobile>
+                </MobileBig>
+
+                <NoMobile>
+                    <Footer></Footer>
+                </NoMobile>
+
+                {/* FIM FOOTER */}
             </div>
         );
     }
---
+++ src/pages/index/sub/servicos_desktop_small/index.js
@@ -0,0 +1,57 @@
+import React from 'react';
+
+import Imagem1 from '../../../../assets/servicos_icones/1.png';
+import Imagem2 from '../../../../assets/servicos_icones/2.png';
+import Imagem3 from '../../../../assets/servicos_icones/3.png';
+import Imagem4 from '../../../../assets/servicos_icones/4.png';
+import Imagem5 from '../../../../assets/servicos_icones/5.png';
+
+import './style.css';
+
+function mapServicos(dados) {
+    return dados.map((servico, key) => {
+        let img;
+        let classes;
+
+        switch (key) {
+            case 0:
+                img = <img height={93} src={Imagem1}></img>;
+                break;
+            case 1:
+                img = <img height={93} src={Imagem2}></img>;
+                break;
+            case 2:
+                img = <img height={93} src={Imagem3}></img>;
+                break;
+            case 3:
+                img = <img height={93} src={Imagem4}></img>;
+                break;
+            case 4:
+                img = <img height={93} src={Imagem5}></img>;
+                break;
+        }
+
+        return (
+            <div className="servico">
+                {img}
+                <p>{servico.nome}</p>
+            </div>
+        );
+    });
+}
+
+export default function Servicos_Resumo({ dados, textos }) {
+    return (
+        <div className="servicos-resumo-s-container">
+            <div className="header">
+                <p className="acima texto-gradiente">{textos.acima}</p>
+                <h2 className="titulo">{textos.titulo}</h2>
+                <p className="paragrafo">{textos.paragrafo}</p>
+            </div>
+            <div className="lista">{mapServicos(dados)}</div>
+            <div className="ver-mais">
+                <p>Ver Mais</p>
+            </div>
+        </div>
+    );
+}
