ab479f9518e1e31d94607f8fa45fe346c498f9f4
matheusAlvarenga <gop.canal@gmail.com>
2020-10-09 11:36:38 -0300

    Finished the Map on Contact Page

--- src/pages/fale_conosco/sub/mapa/index.js
+++ src/pages/fale_conosco/sub/mapa/index.js
@@ -1,17 +1,31 @@
 import React from 'react';
-import { Map } from 'react-leaflet';
+import { Map, TileLayer, Marker, Popup } from 'react-leaflet';
+
+import 'leaflet/dist/leaflet.css';
 
 import './style.css';
 
 export default function Mapa({ dados }) {
-    const position = [51.505, -0.09];
+    const position = { lat: -23.570328, lng: -46.646204 };
     return (
         <div className="mapa-container">
             <Map
                 center={position}
-                zoom={11}
+                zoom={17}
                 style={{ width: '100%', height: '500px' }}
-            ></Map>
+            >
+                <Popup className="pop" position={position}>
+                    <h3 className="titulo">Hubway Agência</h3>
+                    <p className="corpo">
+                        Av Paulista, 171<br></br>
+                        4º Andar
+                    </p>
+                </Popup>
+                <TileLayer
+                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                    attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+                />
+            </Map>
         </div>
     );
 }
