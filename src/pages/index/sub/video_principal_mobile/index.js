794ca448a591329324a43c06b9fe6aec005cb3c7
matheusAlvarenga <gop.canal@gmail.com>
2020-12-23 13:02:51 -0300

    Finished the Hero Changes

--- src/pages/index/sub/video_principal_mobile/index.js
+++ src/pages/index/sub/video_principal_mobile/index.js
@@ -1,11 +1,16 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 import './style.css';
 
 export default function VideoPrincipal() {
     return (
-        <div className="video-principal-container">
-            <div></div>
+        <div className="video-principal-container-m">
+            <div class="textos">
+                <h1>Marketing Digital</h1>
+                <h2>para gerar mais Negócios</h2>
+                <Link to="fale-conosco">FALE COM UM ESPECIALISTA</Link>
+            </div>
         </div>
     );
 }
